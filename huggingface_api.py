ca8b27b5fda0931d22ffeef90977b3d356baa315
Zhou Yang <55669475+yangzhou6666@users.noreply.github.com>
2023-05-10 17:42:27 +0800

    fix: use the correct package name

--- huggingface_api.py
+++ huggingface_api.py
@@ -2,7 +2,7 @@
 
 import requests
 import json
-import yarm
+import yaml
 
 class HuggingFaceAPI:
     def __init__(self, api_key):
@@ -13,13 +13,13 @@ class HuggingFaceAPI:
     def set_interval(self, interval):
         self.interval = interval
 
-    def get_model_list_by_keywords(keyword=None):
-        base_url = "https://huggingface.co"
+    def get_model_list_by_keywords(self, keyword=None):
+        endpoint = "/api/models"
         query_params = {"search": keyword if isinstance(keyword, str) else ""}
-        response = requests.get(base_url + endpoint, query_params)
-        model_list = json.loads(response.content)
+        response = requests.get(self.base_url + endpoint, query_params)
+        response_dict = json.loads(response.content)
 
-        return model_list
+        return response_dict
 
     def get_model_info_by_id(self, model_id):
         endpoint = f"/api/models/{repo_id}"
@@ -33,6 +33,10 @@ class HuggingFaceAPI:
 
 if __name__ == '__main__':
     with open('config.yaml') as f:
-        config = yarm.load(f)
+        config = yaml.load(f)
     # obtain API Key
     api_key = config['huggingface_key']
+
+    hf_api = HuggingFaceAPI(api_key)
+    model_list = hf_api.get_model_list_by_keywords("code")
+    print(len(model_list))
