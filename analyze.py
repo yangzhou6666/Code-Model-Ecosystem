373976eace031d628f570736b3bf006259e91bc1
Zhou Yang <55669475+yangzhou6666@users.noreply.github.com>
2023-05-13 16:48:40 +0800

    feat(analyze); model_data_dependency

--- analyze.py
+++ analyze.py
@@ -37,6 +37,46 @@ def analyze_license(model_dict):
     print(f'Number of unique licenses: {len(license_dict)}')
     print(f'Unique licenses: {license_dict}')
 
+def model_data_dependency(model_dict):
+    total_model = len(model_dict)
+    num_model_with_data_info = 0
+
+    data_dict = {}
+    dependency_dict = {}
+    for model_name in model_dict:
+        model_info = model_dict[model_name]
+        license = None
+        try:
+            tags = model_info['tags']
+        except:
+            print(f'No tags for {model_name}')
+            continue
+        contain_license = False
+        for tag in tags:
+            if 'dataset' in tag:
+                contain_license = True
+                license = tag.split(':')[1]
+
+        if contain_license is False:
+            pass
+        else:
+            num_model_with_data_info += 1
+            if license in data_dict:
+                data_dict[license] += 1
+            else:
+                data_dict[license] = 1
+            dependency_dict[model_name] = license
+
+    print(f'Total model: {total_model}')
+    print(f'Number of models with dataset info: {num_model_with_data_info}')
+    print(f'Number of unique dataset: {len(data_dict)}')
+    print(f'Unique dataset: {data_dict}')
+
+    # save to csv file
+    with open('data/model_data_dependency.csv', 'w') as f:
+        for model_name in dependency_dict:
+            f.write(f'{model_name},{dependency_dict[model_name]}\n')
+
 
 if __name__ == '__main__':
     # read the detailed model info
@@ -45,6 +85,12 @@ if __name__ == '__main__':
         model_detail_dict = json.load(f)
 
     analyze_license(model_detail_dict)
+
+    # read the dataset list
+    with open(f'{save_dir}/dataset_dict.json', 'r') as f:
+        dataset_list = json.load(f)
+    
+    model_data_dependency(model_detail_dict)
 
     
     
