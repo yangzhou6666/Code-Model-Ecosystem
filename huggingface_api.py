a14b986a1598537e159950456c4eafe9a5ccb31d
Zhou Yang <55669475+yangzhou6666@users.noreply.github.com>
2023-05-13 15:24:39 +0800

    feat(data): call HF API and save data

--- huggingface_api.py
+++ huggingface_api.py
@@ -3,6 +3,7 @@
 import requests
 import json
 import yaml
+import time
 
 class HuggingFaceAPI:
     def __init__(self, api_key):
@@ -22,21 +23,62 @@ class HuggingFaceAPI:
         return response_dict
 
     def get_model_info_by_id(self, model_id):
-        endpoint = f"/api/models/{repo_id}"
+        endpoint = f"/api/models/{model_id}"
         headers = {"authorization": "Bearer {}".format(self.api_key)}
 
-        response = requests.get(base_url + endpoint, headers=headers)
+        response = requests.get(self.base_url + endpoint, headers=headers)
+        response_dict = json.loads(response.content)
+
+        return response_dict
+
+    def get_dataset_list(self, keyword=None):
+        endpoint = "/api/datasets"
+        query_params = {"search": keyword if isinstance(keyword, str) else ""}
+        response = requests.get(self.base_url + endpoint, query_params)
         response_dict = json.loads(response.content)
 
         return response_dict
 
 
+def save_json(hf_api, save_dir):
+    # dataset
+    dataset_list = hf_api.get_dataset_list()
+    with open(f'{save_dir}/dataset_list.json', 'w') as f:
+        json.dump(dataset_list, f, indent=4)
+
+    # models with "code" keywords
+    model_list = hf_api.get_model_list_by_keywords("code")
+
+
+    # detail information of each model
+    model_detail_dict = {}
+    count = 0
+    for model in model_list:
+        modelId = model['modelId']
+        model_info = hf_api.get_model_info_by_id(modelId)
+
+        # add model info to dict
+        model_detail_dict[modelId] = model_info
+        time.sleep(0.1)
+        count += 1
+        print('.', end='')
+        if count % 100 == 0:
+            print(f'Finished {count} models')
+
+    # save model info dict
+    with open(f'{save_dir}/model_detail_dict.json', 'w') as f:
+        json.dump(model_detail_dict, f, indent=4)
+
+
+
 if __name__ == '__main__':
+
+    save_dir = 'data'
     with open('config.yaml') as f:
         config = yaml.load(f)
     # obtain API Key
     api_key = config['huggingface_key']
 
     hf_api = HuggingFaceAPI(api_key)
-    model_list = hf_api.get_model_list_by_keywords("code")
-    print(len(model_list))
+    with open(f'{save_dir}/model_list.json') as f:
+        model_list = json.load(f)
