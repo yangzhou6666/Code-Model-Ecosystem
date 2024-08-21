cf6710a996bbea9e28391f27edf33be828130ded
Zhou Yang <55669475+yangzhou6666@users.noreply.github.com>
2024-08-21 14:57:02 +0800

    Update analyze_results_from_llm.py

--- analyze_results_from_llm.py
+++ analyze_results_from_llm.py
@@ -10,15 +10,18 @@ if __name__ == '__main__':
     path_to_labels = './data/manual-labeling.csv'
     model_to_type = get_model_type(path_to_labels)
     
-    prompt_type = 'zero-shot-prompt'
-    openai_model_type = 'gpt-4o-mini'
+    prompt_type = 'few-shot-CoT'
+    # openai_model_type = 'gpt-3.5-turbo'
+    openai_model_type = 'gpt-4o'
     
     # get output file
     output_file = './batches/{}-{}-output.jsonl'.format(prompt_type, openai_model_type)
     assert os.path.exists(output_file), f'{output_file} does not exist'
 
-    
     correct_cnt = 0
+    # count of examples with label 'yes' or 'no'
+    cnt_yes = sum([1 for v in model_to_type.values() if v == 'yes'])
+    cnt_no = sum([1 for v in model_to_type.values() if v == 'no'])
     
     # read the output file
     with open(output_file) as f:
@@ -29,14 +32,15 @@ if __name__ == '__main__':
             ground_truth = model_to_type[model_name]
             
             # process the output
-            llm_output = line['response']["body"]['choices'][0]['message']['content'].lower()
+            if openai_model_type == 'gpt-4o':
+                llm_output = line['content'].lower()
+            else:
+                llm_output = line['response']["body"]['choices'][0]['message']['content'].lower()
             try:
                 answer_region = llm_output.split('final answer')[1]
             except:
                 prediction = 'unknown'
-                
-            print(answer_region)
-                
+                            
             
             # get prediction
             if 'yes' in answer_region:
@@ -50,7 +54,6 @@ if __name__ == '__main__':
                 correct_cnt += 1
     
     print(f'Accuracy: {correct_cnt * 1.0 /len(model_to_type)}')
-    # compute precision
             
             
 
