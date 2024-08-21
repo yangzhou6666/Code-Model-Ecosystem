f8fea8fdf8757aab5a45d8d72f1b4df36446ebef
Zhou Yang <55669475+yangzhou6666@users.noreply.github.com>
2024-08-21 12:16:11 +0800

    refactor: functions to create query

--- build_batch_file.py
+++ build_batch_file.py
@@ -4,32 +4,65 @@ import json
 from pydantic import BaseModel
 
 
-def get_prompt(prompt_type: str):
+def get_query(prompt_type: str, model_name: str, readme: str, openai_model_type: str):
+
     if prompt_type == 'zero-shot-prompt':
         prompt = '''
-            Given the documentation (written in Markdown) of a Hugging Face model, please help me decide whether this model is designed for software engineering related tasks (like code completion, code generation, defect prediction, clone detection, and others). Please provide your answer in either Yes or No Don't provide any other information. Your answer should be in the following template.
+            Given the documentation (written in Markdown) of a Hugging Face model {}, please help me decide whether this model is designed for software engineering related tasks (like code completion, code generation, defect prediction, clone detection, and others). Please provide your answer in either Yes or No Don't provide any other information. Your answer should be in the following template.
 
             Final Answers: Yes or No
 
-            '''
+            '''.format(model_name)
+            
+        question = prompt + '``` \n' + readme + '```'
+    
+        qeury_json = {
+                "custom_id": model_name,
+                "method": "POST",
+                "url": "/v1/chat/completions",
+                "body": {
+                    "model": openai_model_type,
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": question
+                        }
+                    ],
+                    "max_tokens": 1000
+                }
+        }
     elif prompt_type == 'zero-shot-CoT':
         prompt = '''
-        Given the documentation (written in Markdown) of a Hugging Face model, please help me decide whether this model is designed for software engineering related tasks (like code completion, code generation, defect prediction, clone detection, and others). Please explain the reason process of reaching the answer. You may want to analyze the model's training data, tasks, and evaluation dataset. You must provide your final answer in either Yes or No. You cannot say unsure. Your answer should be in the following template.
+        Given the documentation (written in Markdown) of a Hugging Face model {}, please help me decide whether this model is designed for software engineering related tasks (like code completion, code generation, defect prediction, clone detection, and others). Please explain the reason process of reaching the answer. You may want to analyze the model's training data, tasks, and evaluation dataset. You must provide your final answer in either Yes or No. You cannot say unsure. Your answer should be in the following template.
 
         Reason Steps:
 
         Final Answers: Yes or No
         
-        '''
-    elif prompt_type == 'few-shot-CoT':
-        raise NotImplementedError
-        # 这里需要设计一个few-shot的模板
+        '''.format(model_name)
+        
+        question = prompt + '``` \n' + readme + '```'
     
-    
+        qeury_json = {
+                "custom_id": model_name,
+                "method": "POST",
+                "url": "/v1/chat/completions",
+                "body": {
+                    "model": openai_model_type,
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": question
+                        }
+                    ],
+                    "max_tokens": 1000
+                }
+        }
+        
     else:
         raise ValueError(f"Unknown prompt type: {prompt_type}")
-    
-    return prompt
+
+    return qeury_json
 
 def get_model_type(path_to_labels):
     model_to_type = {}
@@ -59,11 +92,10 @@ if __name__ == '__main__':
     path_to_labels = './data/manual-labeling.csv'
     rdm_folder = 'readme-all'
     prompt_type = 'zero-shot-CoT'
-    openai_model_type = 'gpt-3.5-turbo'
+    openai_model_type = 'gpt-4o-mini'
 
     model_to_type = get_model_type(path_to_labels)
     
-    prompt = get_prompt(prompt_type)
     
     # remove the existing file ./batches/{prompt-type}-{model}.jsonl
     if os.path.exists('./batches/{}-{}.jsonl'.format(prompt_type, openai_model_type)):
@@ -83,23 +115,8 @@ if __name__ == '__main__':
             
         with open(os.path.join(rdm_folder, rdm), 'r') as f:
             readme = f.read()
-            question = prompt + '``` \n' + readme + '```'
         
-        qeury_json = {
-                "custom_id": model_name,
-                "method": "POST",
-                "url": "/v1/chat/completions",
-                "body": {
-                    "model": openai_model_type,
-                    "messages": [
-                        {
-                            "role": "user",
-                            "content": question
-                        }
-                    ],
-                    "max_tokens": 1000
-                }
-        }
+            qeury_json = get_query(prompt_type, model_name, readme, openai_model_type)
         
         # write the query to the file
         with open('./batches/{}-{}.jsonl'.format(prompt_type, openai_model_type), 'a') as f:
